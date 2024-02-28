3b2ef022dc151a0bb91ccd1894cb6bb3e955248f
Marchello92 <mark.davidovich@gmail.com>
2024-02-28 11:17:51 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+
+function showGallery() {
+    document.querySelector('.gallery-view').style.display = 'block'
+    document.querySelector('.editor-view').style.display = 'none'
+}
+
+function showEditor() {
+    document.querySelector('.gallery-view').style.display = 'none'
+    document.querySelector('.editor-view').style.display = 'block'
+}
